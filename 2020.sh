4454b49d212b7f26b5abbd8ee7fdfe683ca2bbca
Raptor <raptor.inc2018@gmail.com>
2020-03-22 11:55:37 +0530

    updated script

--- 2020.sh
+++ 2020.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+work_time=$1
+break_time=$2
+if [[ $work_time == "" ]]; then
+	work_time=20
+fi
+if [[ $break_time == "" ]]; then
+	break_time=20
+fi
 while [[ 1 ]]; do
-	sleep 20m
+	sleep ${work_time}m
 	cur_brightness="$(xbacklight -get)"
 	xbacklight -set 0
-	sleep 40
+	sleep $break_time
 	xbacklight -set $cur_brightness
 done
